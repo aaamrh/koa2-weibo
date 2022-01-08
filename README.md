870583306645eb23ff10c843b219302a529c9470
maruihua <marh@qingtong123.com>
2022-01-08 22:06:20 +0800

    refactor: 调整目录结构

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 创建项目: `koa2 -e <name>` , `-e`使用ejs为模板引擎
 
 项目启动: `npm run dev`
+
+## 依赖包
+
+`npm i cross-env -D`
--- src/app.js
+++ src/app.js
@@ -9,14 +9,14 @@ const logger = require('koa-logger')
 const index = require('./routes/index')
 const users = require('./routes/users')
 
-// error handler
+// error handler 页面上显示错误
 onerror(app)
 
 // middlewares
 app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
 }))
-app.use(json())
+app.use(json()) // bodyparser解析完json是string, json()是转换成对象
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
@@ -24,7 +24,7 @@ app.use(views(__dirname + '/views', {
   extension: 'ejs'
 }))
 
-// logger
+// logger 中间件使用演示
 app.use(async (ctx, next) => {
   const start = new Date()
   await next()
@@ -36,7 +36,7 @@ app.use(async (ctx, next) => {
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
 
-// error-handling
+// error-handling 控制台打印错误
 app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
 });
