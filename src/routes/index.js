46a7dbcf47f70eb9e402c692def272673d9052d9
maruihua <marh@qingtong123.com>
2022-02-08 11:28:49 +0800

    feature: 登录验证的中间件

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('koa-router')();
+const { loginRedirect, loginCheck } = require('../middlewares/loginChecks');
 
-router.get('/', async (ctx, next) => {
+
+router.get('/', loginRedirect, async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!'
   });
@@ -10,7 +12,7 @@ router.get('/string', async (ctx, next) => {
   ctx.body = 'koa2 string';
 });
 
-router.get('/json', async (ctx, next) => {
+router.get('/json', loginCheck, async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
   };
