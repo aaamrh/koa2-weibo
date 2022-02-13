580eb5824e802ffb5743696570aca7a04d75a153
maruihua <marh@qingtong123.com>
2022-02-13 15:57:12 +0800

    feature: 用户设置 单元测试

--- test/user/login.test.js
+++ test/user/login.test.js
@@ -50,12 +50,36 @@ test('登录, 应该成功', async () => {
   COOKIE = res.headers['set-cookie'].join(';');
 })
 
+// 修改用户信息
+test('修改基本信息 应该成功', async () => {
+  const res = await server.patch('/api/user/changeInfo').send({
+    nickName: '测试用例',
+    city: "测试城市",
+    picture: "/test.png"
+  }).set('cookie', COOKIE)
+
+  expect(res.body.errno).toBe(0);
+})
+
+test('修改密码 应该成功', async () => {
+  const res = await server.patch('/api/user/changePassword').send({
+    password,
+    newPassword: 'qwe123'
+  }).set('cookie', COOKIE);
+
+  expect(res.body.errno).toBe(0);
+})
+
 // 删除 
 test('删除用户 应该成功', async () => {
   const res = await server.post('/api/user/delete').set('cookie', COOKIE)
   expect(res.body.errno).toBe(0);
 })
 
+test('退出登录 应该成功', async () => {
+  const res = await server.post('/api/user/logout').set('cookie', COOKIE);
+  expect(res.body.errno).toBe(0);
+})
 
 test('查询注册的用户名, 应该不存在 已经被删除', async () => {
   const res = await server.post('/api/user/isExist').send({ userName });
